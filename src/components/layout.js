e8ec53d4d386fd3c4ca5527291f74c7385f6cbf7
David O'Trakoun <me@davidosomething.com>
2019-10-05 03:08:56 -0400

    unused

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,28 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 
-export default ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-
-  return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: rhythm(1),
-      }}
-    >
-      <header>
-        <h1 style={{
-          ...scale(1 / 3),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}>
-          <Link to={`/`} style={{ color: `inherit` }}>{title}</Link>
-        </h1>
-      </header>
-      <main>{children}</main>
-    </div>
-  )
-};
+export default ({ location, title, children }) => (
+  <div
+    style={{
+      marginLeft: `auto`,
+      marginRight: `auto`,
+      maxWidth: rhythm(24),
+      padding: rhythm(1),
+    }}
+  >
+    <header>
+      <h1 style={{
+        ...scale(1 / 3),
+        marginBottom: rhythm(1.5),
+        marginTop: 0,
+      }}>
+        <Link to={`/`} style={{ color: `inherit` }}>{title}</Link>
+      </h1>
+    </header>
+    <main>{children}</main>
+  </div>
+);
