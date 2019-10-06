bb02110ce51d16a7cd83bc531bd9c8f4222f7d82
David O'Trakoun <me@davidosomething.com>
2019-10-06 16:25:37 -0400

    headroom

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { rhythm, scale } from '../utils/typography';
+import Headroom from 'react-headroom';
+import { scale } from '../utils/typography';
 import classes from './layout.module.css';
 
-export default ({ location, title, children }) => (
-  <div className={classes.wrapper}>
-    <header>
-      <h1 style={{
-        ...scale(1 / 3),
-        marginBottom: rhythm(1.5),
-        marginTop: 0,
-      }}>
+export default ({ location, title, children }) => [
+  <Headroom key="header">
+    <header className={classes.mainHeader}>
+      <h1 className={classes.title} style={{ ...scale(1 / 3) }}>
         <Link to={`/`} className={classes.homeLink}>{title}</Link>
       </h1>
     </header>
-    <main>{children}</main>
-  </div>
-);
+  </Headroom>,
+  <main key="main" className={classes.mainWrapper}>{children}</main>
+];
