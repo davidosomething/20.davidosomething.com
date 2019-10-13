0f98ba4e2abe3db382f5667fb1097f0c8b3f26b8
David O'Trakoun <me@davidosomething.com>
2019-10-13 03:05:37 -0400

    Revert "add eslint deps for cli runs"
    This reverts commit 6fc7e050bea3288aec506e61ad3f7dced7b36d6c.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   globals: {
-    graphql: true,
     __PATH_PREFIX__: true,
-    __BASE_PATH__: true, // this will rarely, if ever, be used by consumers
   },
   extends: `react-app`,
-};
+}
