6fc7e050bea3288aec506e61ad3f7dced7b36d6c
David O'Trakoun <me@davidosomething.com>
2019-10-13 02:53:14 -0400

    add eslint deps for cli runs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   globals: {
+    graphql: true,
     __PATH_PREFIX__: true,
+    __BASE_PATH__: true, // this will rarely, if ever, be used by consumers
   },
   extends: `react-app`,
-}
+};
