034c9ae9244e6c58b5e5ec487dda337ff125942a
David O'Trakoun <me@davidosomething.com>
2019-10-25 23:03:52 -0400

    temp commit to move

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Layout } from '../components/layout'
-import { SEO } from '../components/SEO'
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from '../components/layout';
+import { SEO } from '../components/SEO';
 import { PostItem } from '../components/PostItem';
 
 export default ({
