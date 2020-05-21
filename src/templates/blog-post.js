7d4b54371a117a8a6b2eee723ebcb84c623dada2
David O'Trakoun <me@davidosomething.com>
2020-05-21 13:43:35 -0400

    remove utterances, update react-helmet

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,39 +1,33 @@
 import { rhythm, scale } from '../utils/typography';
 import React from 'react';
 import { Link, graphql } from 'gatsby';
-import withUtterances from 'with-utterances';
 import { Layout } from '../components/layout';
 import { SEO } from '../components/seo';
 import classes from './blog-post.module.scss';
 
-const Article = withUtterances(
-  ({ location, frontmatter, post }) => (
-    <article>
-      <header style={{ marginBottom: rhythm(1) }}>
-        <h1 className="page--title">
-          <Link to={location.pathname}>{frontmatter.title}</Link>
-        </h1>
-        <p className={classes.subheader}
-          dangerouslySetInnerHTML={{
-            __html: frontmatter.subheader,
-          }}
-        />
-        <small
-          className={classes.datePublished}
-          style={{ ...scale(-1 / 2) }}
-        >
-          Published on {frontmatter.datePublished}
-        </small>
-      </header>
-      <section
-        className={classes.content}
-        dangerouslySetInnerHTML={{ __html: post.html }}
+const Article = ({ location, frontmatter, post }) => (
+  <article>
+    <header style={{ marginBottom: rhythm(1) }}>
+      <h1 className="page--title">
+        <Link to={location.pathname}>{frontmatter.title}</Link>
+      </h1>
+      <p className={classes.subheader}
+        dangerouslySetInnerHTML={{
+          __html: frontmatter.subheader,
+        }}
       />
-    </article>
-  ),
-  'davidosomething/utterances-davidosomething.com', // repo
-  'github-light', // theme
-  'og:title', // term
+      <small
+        className={classes.datePublished}
+        style={{ ...scale(-1 / 2) }}
+      >
+        Published on {frontmatter.datePublished}
+      </small>
+    </header>
+    <section
+      className={classes.content}
+      dangerouslySetInnerHTML={{ __html: post.html }}
+    />
+  </article>
 );
 
 export default ({ data, location, pageContext }) => {
