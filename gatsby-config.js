17f882d45b7e4a911836be66244c1c3b7e6647e8
David O'Trakoun <me@davidosomething.com>
2020-11-26 00:44:14 -0500

    up deps, dart sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,12 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require('sass')
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
